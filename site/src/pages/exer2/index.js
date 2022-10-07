73f2ae3943826ad7cb4f122b4221120ff9ad9c63
eliza <elizaxavier2@gmail.com>
2022-10-07 19:57:09 -0300

    prova pronta

--- site/src/pages/exer2/index.js
+++ site/src/pages/exer2/index.js
@@ -1,11 +1,35 @@
-import { useState } from 'react'
-import './index.scss'
-export default function Signos() {
-     const [dia, setDia] = useState();
-     const [mes, setMes] = useState();
+import { useEffect, useState } from "react";
 
-     function libra(){
-        
-     }
+export default function Signos(){
+    const [mes, setMes] = useState("");
+    const [dia, setDia] = useState(0);
+    const [resul, setResul] = useState(false);
 
+    function libra(mes, dia) {
+        let resp= false;
+        if(mes ==="Setembro" && dia>22){
+        resp= true;}
+        if(mes ==="Outubro" && dia<=22){
+        resp= true;}
+
+        setResul(resp);
+    }
+
+
+    useEffect( ()=>{
+        libra(mes,dia);
+    },[mes,dia])
+
+    return (
+        <section>
+            <h1>SIGNO LIBRA</h1>
+            <p>MÊS</p>
+            <input type='text' value={mes} onChange={e=>setMes(e.target.value)}/>
+            <p>DIA</p>
+            <input type='number' value={dia} onChange={e=>setDia(e.target.value)}/>
+            <br/>
+            <hr/>
+            <span> VOCÊ É DE LIBRA? {resul? "Sim" : "Não"}</span>
+        </section>
+    );
 }
