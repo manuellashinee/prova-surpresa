cef305ebd3f1b52993a0ef73f73eeda97159cfc4
manuellashinee <manujesus.vasconcelos@gmail.com>
2022-10-07 11:49:38 -0300

    primeiro exercicio pronto

--- site/src/App.js
+++ site/src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+const [qtdPeq, setQtdpeq] = useState();
+const [qtdMed, setQtdMed] = useState();
+const [qtdGra, setQtdGra] = useState();
+const [desc, setDesc] = useState();
+const [ final, setFinal] = useState();
+
+
+
+
+  function custo(){
+    let total = qtdPeq*13.50 + qtdMed*15.00 + qtdGra*17.50;
+    let porcentagem = total*(desc/100);
+    let final = total - porcentagem;
+    setFinal(final);
+
+  }
+  
+    useEffect(() => {
+      custo();
+    
+    }, [final])
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <section>
+          <div className='titulo'>
+            <h1>Preços do Açaí</h1>
+          </div>
+
+    <div className='alinhamento'> 
+    <div className='quadro'>
+      
+      <div className='informacoes'>
+            <p>Qtd.Pequena:</p>
+            <input className='inserir' type='number' value={qtdPeq}  onChange={e => setQtdpeq(Number(e.target.value))}/>
+          </div>
+
+          <div>
+            <p>Qtd.Media:</p>
+            <input type='number' value={qtdMed}  onChange={e => setQtdMed(Number(e.target.value))}/>
+          </div>
+
+          <div>
+            <p>Qtd.Grande:</p>
+            <input type='number' value={qtdGra}  onChange={e => setQtdGra(Number(e.target.value))} />
+          </div>
+
+          <div>
+            <p>Desconto:</p>
+            <input type='number' value={desc}  onChange={e => setDesc(Number(e.target.value))}/>
+          </div>
+
+          <div>
+            <h2>O valor total é: </h2>
+          <button onClick={custo}>Calcular</button>
+
+          {final}
+
+          </div>
+    </div>
+     
+</div>
+
+
+         
+
+          
+
+
+        </section>
+        
+        
       </header>
     </div>
+
   );
 }
 
---
+++ site/src/pages/exer2/index.js
@@ -0,0 +1,4 @@
+import './index.scss'
+export default function Signos() {
+
+}
